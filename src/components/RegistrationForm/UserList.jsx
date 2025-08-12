7f3ebab1ddb67e22de37d674f4b686ecf4fb31b4
Keval2707 <gondaliyakeval07@gmail.com>
2025-08-12 11:54:31 +0530

    Revert "Refactor"
    This reverts commit cc708f3a3866c0a72076d19360ce599b468448fd.

--- src/components/RegistrationForm/UserList.jsx
+++ src/components/RegistrationForm/UserList.jsx
@@ -1,6 +1,7 @@
+import { FiEdit3 } from "react-icons/fi";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 import { useMemo, useState } from "react";
-import { filterUsersByTab, updateUserList } from "./helper";
-import { UserRow } from './../hooks/UserRow';
+import { getAge, updateUserList } from "./helper";
 
 export const UserList = ({
   setUser,
@@ -32,15 +33,19 @@ export const UserList = ({
     }
   };
 
-  const deleteUsers = (ids) => {
-    const updatedList = userList.filter((user) => !ids.includes(user.id));
+  const handleDeleteUser = (id) => {
+    const updatedList = userList.filter((user) => user.id !== id);
     setUserList(updatedList);
     updateUserList(updatedList);
   };
 
-  const handleDeleteUser = (id) => deleteUsers([id]);
   const handleMultipleDelete = () => {
-    deleteUsers(checkedItems.map((item) => item.id));
+    const selectedIds = checkedItems.map((item) => item.id);
+    const updatedList = userList.filter(
+      (user) => !selectedIds.includes(user.id)
+    );
+    setUserList(updatedList);
+    updateUserList(updatedList);
     setCheckedItems([]);
     setSelect(false);
   };
@@ -50,10 +55,14 @@ export const UserList = ({
     setIsEditing(true);
   };
 
-  const currentUserList = useMemo(
-    () => filterUsersByTab(userList, activeTab),
-    [activeTab, userList]
-  );
+  const currentUserList = useMemo(() => {
+    if (activeTab === "ActiveUsers") {
+      return userList.filter((user) => user.active);
+    } else if (activeTab === "InactiveUsers") {
+      return userList.filter((user) => !user.active);
+    }
+    return userList;
+  }, [activeTab, userList]);
 
   console.log("userList", userList);
 
@@ -79,20 +88,164 @@ export const UserList = ({
         </thead>
 
         <tbody>
-          {currentUserList.map((user) => (
-            <UserRow
-              key={user.id}
-              user={user}
-              select={select}
-              checkedItems={checkedItems}
-              handleSelect={handleMultipleSelectedUser}
-              handleEdit={handleEdit}
-              handleDelete={handleDeleteUser}
-              isEditing={isEditing}
-              activeTab={activeTab}
-              handleActiveUser={handleActiveUser}
-            />
-          ))}
+          {currentUserList.map(
+            ({
+              id,
+              dob,
+              dod,
+              name,
+              email,
+              active,
+              gender,
+              hobbies,
+              password,
+              programming,
+              confirmPassword,
+            }) => {
+              return (
+                <tr
+                  key={id}
+                  className={`${
+                    !active && activeTab !== "InactiveUsers"
+                      ? "line-through"
+                      : ""
+                  }`}
+                >
+                  {select ? (
+                    <td>
+                      <input
+                        id={id}
+                        name={id}
+                        value={id}
+                        checked={checkedItems.some((i) => i.id === id)}
+                        type="checkbox"
+                        onChange={(event) =>
+                          handleMultipleSelectedUser(event, {
+                            id: id,
+                            dob,
+                            name,
+                            email,
+                            gender,
+                            hobbies,
+                            password,
+                            programming,
+                            confirmPassword,
+                          })
+                        }
+                      />
+                    </td>
+                  ) : null}
+
+                  <td>{id}</td>
+
+                  <td>
+                    <div
+                      className="flex justify-between cursor-pointer"
+                      onClick={() => handleActiveUser(id)}
+                    >
+                      <span>{name}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{email}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{gender}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{dob}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-center">
+                      <span>{dod === "" ? "-----" : dod}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{getAge(dob, dod)}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{programming}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{password}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <div className="flex justify-between">
+                      <span>{hobbies}</span>
+                    </div>
+                  </td>
+
+                  <td>
+                    {isEditing || select ? (
+                      <button
+                        disabled
+                        className="cursor-not-allowed text-gray-500"
+                      >
+                        <RiDeleteBin6Fill />
+                      </button>
+                    ) : (
+                      <button
+                        className="cursor-pointer"
+                        onClick={() => handleDeleteUser(id)}
+                      >
+                        <RiDeleteBin6Fill />
+                      </button>
+                    )}
+                  </td>
+
+                  <td>
+                    {isEditing || select ? (
+                      <button
+                        disabled
+                        className="cursor-not-allowed text-gray-500"
+                      >
+                        <FiEdit3 />
+                      </button>
+                    ) : (
+                      <button
+                        className="cursor-pointer"
+                        onClick={() =>
+                          handleEdit({
+                            id,
+                            dob,
+                            name,
+                            email,
+                            gender,
+                            hobbies,
+                            password,
+                            programming,
+                            confirmPassword,
+                          })
+                        }
+                      >
+                        <FiEdit3 />
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            }
+          )}
         </tbody>
       </table>
 
